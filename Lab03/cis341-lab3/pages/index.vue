68c254bbfb17638c6bb7b2a4a48fe011f57a532c
AnikaSardar <asard395@uwsp.edu>
2024-09-25 23:10:21 -0500

    lab03 assignment on NUXT tutorial

---
+++ Lab03/cis341-lab3/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <header>
+            <nav class="nav-bar">
+                <NuxtLink to="/" class="logo">FoodieHub</NuxtLink>
+                <ul>
+                    <li><NuxtLink to="/">Home</NuxtLink></li>
+                    <li><NuxtLink to="/about">About</NuxtLink></li>
+                </ul>
+            </nav>
+        </header>
+        <h2>Welcome Foodie!</h2>
+        <p>FoodieHub welcomes you to your one-stop food destination.</p>
+        
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+@import url('~/assets/css/first.css');
+</style>
