730481b31c44bd30d531269e7ec2ea63dab6ae09
AnikaSardar <asard395@uwsp.edu>
2024-12-06 20:00:26 -0600

    add login and unauthorized path

--- cis341-lab08/cis341-mockAPIEndpoints/layouts/login.vue
+++ cis341-lab08/cis341-mockAPIEndpoints/layouts/login.vue
@@ -17,12 +17,22 @@
       </form>
       <p v-if="error" style="color: red;">{{ error }}</p>
     </div>
-
-        <NuxtPage />
+    
     </div>
   </template>
 
 <script setup>
+useHead({
+  link: [
+    {
+      rel: 'stylesheet',
+      href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+      integrity: 'sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH',
+      crossorigin: 'anonymous'
+    }
+  ]
+})
+
 const title = "Login/Sign Up"
 
 const username = ref('');
