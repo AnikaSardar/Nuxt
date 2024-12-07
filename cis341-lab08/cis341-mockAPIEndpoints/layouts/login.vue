d8af1f47f0e1f6c671a336211df5f5a29230d06c
AnikaSardar <asard395@uwsp.edu>
2024-12-06 23:35:19 -0600

    login css

--- cis341-lab08/cis341-mockAPIEndpoints/layouts/login.vue
+++ cis341-lab08/cis341-mockAPIEndpoints/layouts/login.vue
@@ -1,8 +1,8 @@
 <template>
-    <div>
-      <title>{{ title }}</title>
+  <div class="login-container">
+    <title>{{ title }}</title>
 
-      <div class="col-3">
+    <div class="login-form-container">
       <h1>Login</h1>
       <form @submit.prevent="handleLogin">
         <div class="mb-3">
@@ -17,9 +17,8 @@
       </form>
       <p v-if="error" style="color: red;">{{ error }}</p>
     </div>
-    
-    </div>
-  </template>
+  </div>
+</template>
 
 <script setup>
 useHead({
@@ -36,25 +35,26 @@ useHead({
 const title = "Login/Sign Up"
 
 const username = ref('');
-  const password = ref('');
-  const error = ref('');
-  const { login } = useAuth();
-  const router = useRouter();
-  
-  // Login function
-  const handleLogin = async () => {
-    try {
-      const success = await login(username.value, password.value);
-      // Redirect to home page after successful login
-      if(success) {
-        await navigateTo('/');
-      }
-    } catch (err) {
-      console.log(err);
-      error.value = err.message;  // Show error if login fails
+const password = ref('');
+const error = ref('');
+const { login } = useAuth();
+const router = useRouter();
+
+// Login function
+const handleLogin = async () => {
+  try {
+    const success = await login(username.value, password.value);
+    // Redirect to home page after successful login
+    if(success) {
+      await navigateTo('/');
     }
-  };
+  } catch (err) {
+    console.log(err);
+    error.value = err.message;  // Show error if login fails
+  }
+};
 </script>
 
 <style scoped>
+@import url("~/assets/css/login.css");
 </style>
